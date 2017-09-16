7c30fa0886ee2774c897eab837e6910ab875865d
Rgstaub <rgstaub@gmail.com>
2017-09-16 15:30:54 -0400

    set up server.js and slightly modified the config.json (set dev password as "root" and names db "jumanji"

---
+++ server.js
@@ -0,0 +1,33 @@
+
+// =====================| Server |==============================
+
+// Set up dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+
+// Create the app and set the port
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Requiring our models for syncing
+var db = require("./models");
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// Static directory
+app.use(express.static("public"));
+
+// Call our routes
+require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
+
+// Sync the sequelize models and start the app
+db.sequelize.sync({}).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
