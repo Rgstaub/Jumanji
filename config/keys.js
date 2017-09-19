c9ff177f6027583eea120050fdb252abd3ed349b
Rgstaub <rgstaub@gmail.com>
2017-09-19 16:35:48 -0400

    Delete keys.js
    Removed the keys.js file so that we can keep our twilio keys private

--- config/keys.js
+++
@@ -1,8 +0,0 @@
-// This module will be used to authenticate to the Twilio SMS API, and will be added to the .gitignore file
-const keys = {
-    twilio: {
-        accountSid: 'AC296951b3e5068ef732673e1da02915ab',
-        authToken: '36de9c1c68852474790adc8d55e414e3'
-    }
-}
-module.exports = keys;
