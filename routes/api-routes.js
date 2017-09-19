563ba422c9c503d65186aca10c1c43e2fc7936af
Rgstaub <rgstaub@gmail.com>
2017-09-19 14:45:16 -0400

    texting api is working

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,4 +1,6 @@
 
+const sms = require('../config/twilio');
+const senderNumber = '+19802220114';
 
 
 //========================| API Routes |======================
@@ -6,4 +8,15 @@
 module.exports = function(app) {
   // placeholder code
   console.log("API routes have connected");
+
+  app.get('/api', (req, res) => {
+    console.log("ROUTED");
+    sms.messages.create({
+      body: 'TEST MESSAGE',
+      to: '+17049062487',  // For testing - replace this with data from the user account later
+      from: senderNumber
+    })
+    .then((message) => res.send(message.sid));
+  })
+
 }
