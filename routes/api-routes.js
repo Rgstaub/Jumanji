85c4ef0d75bece53527555af442da7ece770d0bb
Rgstaub <rgstaub@gmail.com>
2017-09-20 00:16:33 -0400

    small updates

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -8,11 +8,11 @@ module.exports = function(app) {
   // placeholder code
   console.log("API routes have connected");
 
-  app.get('/api', (req, res) => {
-    console.log("ROUTED");
+  app.get('/test', (req, res) => {
+    console.log("Test SMS sent");
     sms.messages.create({
-      body: 'HI DZMITRY :D',
-      to: '+17046146046',  // For testing - replace this with data from the user account later
+      body: 'Richard: your turn!',
+      to: '+17049062487',  // For testing - replace this with data from the user account later
       from: senderNumber
     })
     .then((message) => res.send(message.sid));
