992bea888280b5031b3b89ae783dbc3132f0c607
Rgstaub <rgstaub@gmail.com>
2017-09-28 16:55:21 -0400

    menus are all working

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -16,15 +16,52 @@ $(document).ready(function() {
     $.post(userIdStr).done(function() {
       let resumeUrl = "/resumegames/" + userId;
       console.log(resumeUrl);
-      $.get(resumeUrl).done(function(response){
-        console.log(response);
+      $.get(resumeUrl).done(function(gameArr){
+        console.log(gameArr);
+        gameArr.forEach(function(game) {
+
+          var gamePanel = $(`<div class="panel panel-default resume-game fixed" data-playerId="${game.myPlayerId}"></div>`);
+          var name = $(`<div class="panel panel-heading"></div>`);
+          name.text(game.name);
+          gamePanel.append(name);
+          var players = $(`<div class="panel panel-body">Players in game:</div>`);
+          if (game.gameState === "waiting") {
+            var available = $(`<div class="panel panel-body"></div>`);
+            available.text(`Open Spots: ${game.available}`);
+            gamePanel.append(available);
+          } else {
+            var turn = $(`<div class="panel panel-body"></div>`);
+            turn.text(`Turn: ${game.turn}`);
+            gamePanel.append(turn);
+          }
+          var playerName = ""
+          game.players.forEach(function(player){
+              playerName = $(`<p>${player}</p>`);
+              players.append(playerName);
+          })
+          
+          
+          gamePanel.append(players)
+          $("#resumable").append(gamePanel);
+        })
       })
     })
   }
 
-  // $('#createGame').click(function() {
-  //   event.preventDefault();
+  $(document).on('click', '.resume-game', function() {
+    event.preventDefault();
+    var playerId = $(this).attr("data-playerId");
+    sessionStorage.setItem("jumanjiPlayerId", playerId);
 
-  // })
+    var resumeUrl = "/loadturn/" + playerId;
+    $.get(resumeUrl).done(function(response) {
+      console.log(response);
+      var redirectUrl = "http://" + window.location.hostname + ":" + window.location.port + "/jumanji.html";
+      console.log(redirectUrl);
+      window.location.replace(redirectUrl);
+    })
+  })
+
+
 
 })
