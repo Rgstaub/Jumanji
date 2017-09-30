75891d415527d40916405f1b7609b3ff94c6733d
Angela <agpolly@gmail.com>
2017-09-30 00:32:22 -0400

    final touches

--- models/users.js
+++ models/users.js
@@ -16,3 +16,11 @@ module.exports = function(sequelize, DataTypes) {
 
   return users;
 }
+
+// function formatPhoneNumber(s) {
+//   var s2 = (""+s).replace(/\D/g, '');
+//   var m = s2.match(/^(\d{3})(\d{3})(\d{4})$/);
+//   return (!m) ? null : "(" + m[1] + ") " + m[2] + "-" + m[3];
+// }
+
+// formatPhoneNumber();
